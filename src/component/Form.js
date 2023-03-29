9a7efa11b35cf2b994c942a92de04c9d8769c0f0
Fausio Matsinhe <fausio.matsinhe@kooli.io>
2023-03-29 17:28:23 +0200

    form part 1

--- src/component/Form.js
+++ src/component/Form.js
@@ -1,9 +1,29 @@
 function Form() {
-    return ( 
+
+    const myStyle =  {
+        button:  {
+
+            "margin": "1px"
+        }
+
+    }
+
+    return (
         <div>
-            <label> forms </label>
+            <form>
+                <input type="text" placeholder="Name" />
+                <br />
+                <input type="text" placeholder="Brand" />
+                <br />
+                <br />
+
+                <input style={myStyle.button} type="button" value="Create" />
+                <input style={myStyle.button} type="button" value="Update" />
+                <input style={myStyle.button} type="button" value="Delete" />
+                <input style={myStyle.button} type="button" value="Cancel" />
+            </form>
         </div>
-     );
+    );
 }
 
 export default Form;
