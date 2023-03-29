71b950d40550d0d6a4861ad9aca2b0f52bee9f24
Fausio Matsinhe <fausio.matsinhe@kooli.io>
2023-03-29 17:45:21 +0200

    css forms

--- src/component/Form.js
+++ src/component/Form.js
@@ -13,16 +13,15 @@ function Form() {
     return (
         <div>
             <form >
-                <input className="form-control" style={myStyle.input} type="text" placeholder="Name" />
-                <br />
-                <input className="form-control" style={myStyle.input} type="text" placeholder="Brand" />
-                <br />
-                <br />
+                <input className="form-control"   type="text" placeholder="Name" />
+                
+                <input className="form-control"  type="text" placeholder="Brand" />
+             
 
-                <input className="btn btn-primary" style={myStyle.button} type="button" value="Create" />
-                <input className="btn btn-warning" style={myStyle.button} type="button" value="Update" />
-                <input className="btn btn-danger" style={myStyle.button} type="button" value="Delete" />
-                <input className="btn btn-secondary" style={myStyle.button} type="button" value="Cancel" />
+                <input className="btn btn-primary"   type="button" value="Create" />
+                <input className="btn btn-warning"   type="button" value="Update" />
+                <input className="btn btn-danger"   type="button" value="Delete" />
+                <input className="btn btn-secondary"  type="button" value="Cancel" />
             </form>
         </div>
     );
