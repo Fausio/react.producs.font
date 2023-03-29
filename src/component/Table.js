348b2f211c685ff656155d55bad95c9373670535
Fausio Matsinhe <fausio.matsinhe@kooli.io>
2023-03-29 17:30:50 +0200

    table part 1

--- src/component/Table.js
+++ src/component/Table.js
@@ -1,9 +1,26 @@
 function Table() {
-    return ( 
+    return (
         <div>
-            <label> Table </label>
+            <table border={1}>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Brand</th>
+                        <th>Options</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>#</td>
+                        <td>#</td>
+                        <td>#</td>
+                        <td>#</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
-     );
+    );
 }
 
 export default Table;
